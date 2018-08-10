c27785df9c5f26bd94c90b2fa3ce0e802d29f6a9
Timothy Gu <timothygu@chromium.org>
2018-08-10 12:03:38 -0700

    Implement ignore-opens-during-unload counter in Document
    The removed test fast/loader/document-destruction-within-unload.html was
    originally added in 8809405d796cf3023e26cefd4b06f369eb67f125, to make
    sure that a document.write() call that blows away the current page still
    works in unload event listeners and blocks any current attempt to
    navigate. Since we no longer allow that to happen, the test is obsolete.
    In fact, it can be seen as contradictory to
    external/wpt/html/browsers/browsing-the-web/unloading-documents/005.html,
    which tests that document.open() in an unload event doesn't block
    navigation.
    
    Bug: 583586, 866274
    Change-Id: I99b35dd28c97e8603455805b31d49644bc7b23a5

--- html/webappapis/dynamic-markup-insertion/opening-the-input-stream/ignore-opens-during-unload.window.js
+++ html/webappapis/dynamic-markup-insertion/opening-the-input-stream/ignore-opens-during-unload.window.js
@@ -1,4 +1,4 @@
-for (const ev of ["unload", "beforeunload", "pagehide"]) {
+for (const ev of ["beforeunload", "pagehide", "unload"]) {
   async_test(t => {
     const iframe = document.body.appendChild(document.createElement("iframe"));
     t.add_cleanup(() => iframe.remove());
@@ -11,5 +11,24 @@ for (const ev of ["unload", "beforeunload", "pagehide"]) {
       }));
       iframe.src = "about:blank";
     });
-  }, `document.open should bail out when ignore-opens-during-unload is greater than 0 during ${ev} event`);
+  }, `document.open should bail out when ignore-opens-during-unload is greater than 0 during ${ev} event (in top-level browsing context)`);
+
+  async_test(t => {
+    const iframe = document.body.appendChild(document.createElement("iframe"));
+    t.add_cleanup(() => iframe.remove());
+    iframe.src = "/common/blank.html";
+    iframe.onload = t.step_func(() => {
+      const doc = iframe.contentDocument;
+      const innerIframe = doc.body.appendChild(doc.createElement("iframe"));
+      innerIframe.src = "/common/blank.html";
+      innerIframe.onload = t.step_func(() => {
+        innerIframe.contentWindow.addEventListener(ev, t.step_func_done(() => {
+          assert_not_equals(iframe.contentDocument.childNodes.length, 0);
+          iframe.contentDocument.open();
+          assert_not_equals(iframe.contentDocument.childNodes.length, 0);
+        }));
+        iframe.src = "about:blank";
+      });
+    });
+  }, `document.open should bail out when ignore-opens-during-unload is greater than 0 during ${ev} event (in descendant browsing context)`);
 }
